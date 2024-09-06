866d12a027be203a644b83a50065d8fba65aca8b
Mateo <manosalvas.mateof@gmail.com>
2024-09-05 19:12:58 -0500

    Create the methods to copy the transcription to the clipboard and to download it in .txt and .doc format.

--- front.py
+++ front.py
@@ -1,43 +1,53 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_file
 from src.pdf_reader.service.ReaderService import ReaderService
 import os
-from dotenv import load_dotenv
-from src.nlp.service.InferenceService import InferenceService
 from werkzeug.utils import secure_filename
+from io import BytesIO
+from docx import Document
 
 app = Flask(__name__)
 
-@app.errorhandler
-def nor_foud(error):
-    return 'Ruta no encontrada'
+@app.errorhandler(404)
+def not_found(error):
+    return 'Ruta no encontrada', 404
 
-@app.route('/', methods= ['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def home():
     return render_template('home.html')
 
-@app.route('/resume&answer', methods= ['GET', 'POST'])
-def resumirYContestar():
-    load_dotenv("secrets/.env")
-    if request.method == 'POST':
-        file = request.files['archivo']
-        basePath = os.path.dirname(__file__)
-        filename = secure_filename(file.filename)
-        if os.path.splitext(filename)[1] == ".pdf":
-            # Para cargar el archivo en la carpeta resources
-            uploadPath = os.path.join(basePath, 'src/pdf_reader/resources', filename)
-            file.save(uploadPath)
-            analisisPath = os.path.join('src/pdf_reader/resources', filename)
-            # return '<br><br><center>El registro fue un exito<br><br><center>'
-            question = str(request.form.get('quest'))
-            texto = ReaderService.pdf2txt1page(analisisPath, 8)
-            resumen = InferenceService().invokeResume(texto)
-            respuesta = InferenceService().invokeCustom(question, texto)
-            mostrar = True
-            return render_template('home.html', resumen=resumen, question=question, respuesta=respuesta, mostrar=mostrar)
-            '''return (f'<h1>Metadatos: {ReaderService.meta(analisisPath)}</h1>'
-                f'<h1>Numero de paginas: {ReaderService.pageN(analisisPath)}</h1>')'''
-        return '<br><br><center>Debe ser formato pdf<br><br><center>'
-    return render_template('home.html')
+@app.route('/transcript', methods=['POST'])
+def transcribir():
+    if 'archivo' not in request.files:
+        return '<br><br><center>No se ha seleccionado ningún archivo<br><br><center>'
+    file = request.files['archivo']
+    if file.filename == '':
+        return '<br><br><center>No se ha seleccionado ningún archivo<br><br><center>'
+    basePath = os.path.dirname(__file__)
+    filename = secure_filename(file.filename)
+    if os.path.splitext(filename)[1] == ".pdf":
+        # Para cargar el archivo en la carpeta resources
+        uploadPath = os.path.join(basePath, 'src/pdf_reader/resources', filename)
+        file.save(uploadPath)
+        analisisPath = os.path.join('src/pdf_reader/resources', filename)
+        trascription = ReaderService.pdf2str(analisisPath)
+        mostrar = True
+        return render_template('home.html', trascription=trascription, mostrar=mostrar)
+    return '<br><br><center>Debe ser formato pdf<br><br><center>'
+
+@app.route('/download_txt', methods=['POST'])
+def download_txt():
+    text = request.form['transcription']
+    return send_file(BytesIO(text.encode()), as_attachment=True, download_name='transcription.txt', mimetype='text/plain')
+
+@app.route('/download_docx', methods=['POST'])
+def download_docx():
+    text = request.form['transcription']
+    doc = Document()
+    doc.add_paragraph(text)
+    buffer = BytesIO()
+    doc.save(buffer)
+    buffer.seek(0)
+    return send_file(buffer, as_attachment=True, download_name='transcription.docx', mimetype='application/vnd.openxmlformats-officedocument.wordprocessingml.document')
 
 if __name__ == '__main__':
-    app.run(host='localhost',port=5000,debug=True);
+    app.run(host='localhost', port=5000, debug=True)
--- main.py
+++ main.py
@@ -1,27 +1,17 @@
-from src.pdf_reader.service.ReaderService import ReaderService
-import os
-from dotenv import load_dotenv
-from src.nlp.service.InferenceService import InferenceService
-
-if __name__ == '__main__':
-    # Definimos un nombre para nuestra clase de lectura
-    reader = ReaderService
-    # Definimos el nombre del archivo pdf en la carpeta resources
-    archivo = "src/pdf_reader/resources/ATanembaumSOModernos.pdf"
-    # Llamamos a la función que nos dice el número de páginas del pdf
-    # print(reader.pageN(archivo))
-    # Llamamos a la función que nos dice la metadata de páginas del pdf
-    # print(reader.meta(archivo))
-    # Llamamos a la función que extrae la informacion del pdf a txt
-    # reader.pdf2txt(archivo)
-    # Llamamos a la función que imprime la informacion de 1 pagina del pdf
-    # print(reader.pdf2txt1page(archivo,5))
-    # Cargamos la informacion de nuestro token de openAI
-    # load_dotenv("secrets/.env")
-    # Imprimimos la API_KEY
-    # print(os.getenv('OPENAI_API_KEY'))
-    # Imprimimos el resultado de nuestro prompt
-    # print(InferenceService().invokeResume(reader.pdf2txt1page(archivo,50)))
-    # prompt = input()
-    # print(InferenceService().invokeCustom(prompt, reader.pdf2txt1page(archivo,50)))
-    # print(reader.pdf2str(archivo))
+# from src.pdf_reader.service.ReaderService import ReaderService
+#
+# if __name__ == '__main__':
+#     # Definimos un nombre para nuestra clase de lectura
+#     reader = ReaderService
+#     # Definimos el nombre del archivo pdf en la carpeta resources
+#     archivo = "src/pdf_reader/resources/Archivo.pdf"
+#     # Llamamos a la función que nos dice el número de páginas del pdf
+#     npages = reader.pageN(archivo)
+#     # Llamamos a la función que nos dice la metadata de páginas del pdf
+#     # print(reader.meta(archivo))
+#     # Llamamos a la función que extrae la informacion del pdf a txt
+#     # reader.pdf2txt(archivo)
+#     # Llamamos a la función que imprime la informacion de todas las paginas del pdf
+#     i = 0
+#     for i in range(npages):
+#         print(reader.pdf2txt1page(archivo,i))
